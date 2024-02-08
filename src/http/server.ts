947dc505e2e4ff030d083814db936eec1650d2f2
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2024-02-08 18:06:46 -0300

    add db

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,12 @@ import fastify from 'fastify'
 
 const app = fastify()
 
+app.post('/polls', () => {})
+
+app.get('/polls/:id', () => {})
+
+app.post('/polls/:id/votes', () => {})
+
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server running')
 })
