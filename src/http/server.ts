9c7753380e7c884bafabbd4d83a1f100ed0edb05
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2024-02-08 18:20:26 -0300

    add create poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,20 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.post('/polls', () => {})
+app.post('/polls', async (req, reply) => {
+  const createPollBody = z.object({
+    title: z.string()
+  })
+  const { title } = createPollBody.parse(req.body)
+
+  const poll = await prisma.poll.create({ data: { title } })
+
+  return reply.status(201).send({ pollID: poll.id })
+})
 
 app.get('/polls/:id', () => {})
 
